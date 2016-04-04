891a6af3c3699c780714e534997c211eb0b14c09
Tanaka Tomohiro <ttanaka@dhcp00.kbys.ip.titech.ac.jp>
2016-04-04 23:53:21 +0900

    Modified REVERB_create_dtetdata.sh to avoid depending on directory structure.

--- egs/reverb/s5/local/REVERB_create_dtetdata.sh
+++ egs/reverb/s5/local/REVERB_create_dtetdata.sh
@@ -5,10 +5,10 @@
 ## Creating REVERB data and directory structure.
 ## (Real data and simulated data for dev and eval sets)
 
-if [ -d $REVERB_home ]; then
- echo "Not creating REVERB data since it is already there."
- exit 0
-fi
+#if [ -d $REVERB_home ]; then
+# echo "Not creating REVERB data since it is already there."
+# exit 0
+#fi
 
 if [ $# -ne 2 ]; then
   echo "\nUSAGE: %s <wsjcam0 data dir> <multi-channel wsj data dir>\n"
@@ -23,7 +23,7 @@ if [ ! `which flac` ]; then
 fi
 
 wsjcam0=$1
-multi_wsj=$2/LDC2014S03_d2
+multi_wsj=$2
 dir=`pwd`/data/local/reverb_tools
 
 # Download tools
@@ -59,15 +59,21 @@ mkdir -p $reverb_real_dt/{audio/stat,etc,mlf} $reverb_real_et/{audio/stat,mlf}
 
 # Copy data and creating directory structure
 echo "Copy and create directory structure."
-find $multi_wsj \( -name T? -or -name T10 \) | xargs cp -r -t $reverb_real_dt/audio/stat
-find $multi_wsj \( -name T3? -or -name T40 \) | xargs cp -r -t $reverb_real_et/audio/stat
-find $multi_wsj -name sentencelocation -print | xargs cp -r -t $reverb_real_dt/etc
-find $multi_wsj -name WSJ.mlf | xargs cp -r -t $reverb_real_dt/mlf
-find $multi_wsj -name WSJ.mlf | xargs cp -r -t $reverb_real_et/mlf
+
+stat=`find $wsj_multi -type d -name stat` 
+for y in `seq 6 10`; do
+    find $stat -name T$y | xargs cp -r -t $reverb_real_dt/audio/stat
+done
+for z in `seq 21 25` `seq 36 40`; do
+    find $stat -name T$z | xargs cp -r -t $reverb_real_et/audio/stat
+done
+find $wsj_multi -name sentencelocation -print | xargs cp -r -t $reverb_real_dt/etc
+find $wsj_multi -name WSJ.mlf | xargs cp -r -t $reverb_real_dt/mlf
+find $wsj_multi -name WSJ.mlf | xargs cp -r -t $reverb_real_et/mlf
 
 # Convert .flac file to .wav file
 echo "Convert .flac file to .wav file."
 echo "Log file is in $dir/flac.log"
-flac -d -f --delete-input-file {$reverb_real_dt,$reverb_real_et}/audio/stat/T*/*/*.flac > flac.log 2>&1
+flac -d -f --delete-input-file {$reverb_real_dt,$reverb_real_et}/audio/stat/T*/*/*/*.flac > $dir/flac.log 2>&1
 
 echo "Successfully generated Real data." && exit 0;
